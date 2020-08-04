39eab78e96c827b55ac743ca2e0c06db01e178f0
Jacco Ekelo <none>
2020-08-05 00:18:33 +0200

    Initial Commit

---
+++ udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  udder
+//
+//  Created by Jacco Ekelo on 05/08/2020.
+//  Copyright © 2020 Jacco Ekelo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
